92c030611bad84dfb5a4399a8be2f6dac39e0167
Javed <javedahmednizamani@gmail.com>
2023-11-01 07:04:27 +0500

    1/11/2023

---
+++ assets/scripts/app.js
@@ -0,0 +1,6 @@
+const defaultResult = 0;
+let currentResult = defaultResult;;
+calculationDescription = '('+currentResult + ' + 10) * 3/2-1'
+
+currentResult = (currentResult + 10) * 3/2-1;
+outputResult(currentResult, calculationDescription);
