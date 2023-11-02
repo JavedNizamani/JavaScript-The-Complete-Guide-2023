fd9c2f16deeac3a35837764e19037d19dae486a5
Javed <javedahmednizamani@gmail.com>
2023-11-03 00:41:28 +0500

    simple_Calculator

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,46 @@
 const defaultResult = 0;
-let currentResult = defaultResult;;
-calculationDescription = '('+currentResult + ' + 10) * 3/2-1'
+let currentResult = defaultResult;
 
-currentResult = (currentResult + 10) * 3/2-1;
-outputResult(currentResult, calculationDescription);
+//Gets input from input field
+const getUserInputNumber = ()=>{
+     return parseInt(userInput.value);
+}
+//Write Calculation Log
+const createAndWriteOutput = (operator, resultBeforeCalc, calcNumber)=>{
+     const clacDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+     outputResult(currentResult, clacDescription);          // from vendor file
+}
+
+const add = ()=>{
+     const enteredNumber = getUserInputNumber();
+     const initialResult = currentResult;
+     currentResult = currentResult + enteredNumber;
+     createAndWriteOutput('+', initialResult, enteredNumber);
+     
+}
+
+const subtract = ()=>{
+     const enteredNumber = getUserInputNumber();
+     const initialResult = currentResult;
+     currentResult = currentResult - enteredNumber;
+     createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+const multiply = ()=>{
+     const enteredNumber = getUserInputNumber();
+     const initialResult = currentResult;
+     currentResult = currentResult * enteredNumber;
+     createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+const divide = ()=>{
+     const enteredNumber = getUserInputNumber();
+     const initialResult = currentResult;
+     currentResult = currentResult / enteredNumber;
+     createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
+addBtn.addEventListener('click',add);
+subtractBtn.addEventListener('click',subtract);
+multiplyBtn.addEventListener('click',multiply);
+divideBtn.addEventListener('click',divide);
